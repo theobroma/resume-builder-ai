8652a24f1a3ace6bce8fc7729f0ea7d8f4a6040d
Alexandr Siryj <theobroma222@gmail.com>
2023-02-24 20:55:30 +0200

    react-to-print

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Resume from "./components/Resume";
 
 const App = () => {
+    //👇🏻 state holding the result
+    const [result, setResult] = useState({});
+
     return (
         <div>
             <BrowserRouter>
